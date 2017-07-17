68f380d03d1029105cdfd33bdf35441a19d895ea
Sireesh P <Sireesh P>
2017-07-17 15:01:09 +0100

    removed service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 export default function initReactAPP(id, schema) {
@@ -11,6 +10,4 @@ export default function initReactAPP(id, schema) {
     </BrowserRouter>,
     document.getElementById(id)
   );
-
-  registerServiceWorker();
 }
